56cbb0167165c0423dccfd47f218040e99dc5b21
Nahuel <nahulisas@gmail.com>
2023-12-06 19:25:13 -0300

    primer commit

---
+++ condicionales/practica.py
@@ -0,0 +1,23 @@
+numero = int(input("Adivina en que numero estoy pensando "))
+print("Chequeando si son iguales")
+if numero == 7:
+    print("Adivinaste")
+print("Chequeando si es menor")
+if numero < 7:
+    print("te quedaste corto!")
+print("Chequeando si es mayor")
+if numero > 7:
+    print("te pasaste!")
+
+
+print("Chequeando si son iguales")
+if numero == 7:
+    print("Adivinaste")
+else:
+    print("Chequeando si es menor")
+    if numero < 7:
+        print("te quedaste corto!")
+    else:
+        print("te pasaste!")
+
+
