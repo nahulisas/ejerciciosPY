48c8054ff135ee589038bf77ec5715ffaae7f836
Nahuel <nahulisas@gmail.com>
2023-12-07 10:22:28 -0300

    agregado ej7

--- condicionales/ejercicio7.py
+++ condicionales/ejercicio7.py
@@ -1,2 +1,8 @@
 num1 = float(input("Ingrese un numero: "))
-num1 = float(input("Ingrese el segundo numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+resultado = (num1 + num2) / 2
+if resultado >= 7:
+    print("El promedio de tus notas es: ",resultado," aprobaste")
+else:
+    print("El promedio de tus notas es ",resultado," desaprobado")
+
