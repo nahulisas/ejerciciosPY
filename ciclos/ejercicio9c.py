3df5a641472f284f9fc252aba38588dea5e901b5
Nahuel <nahulisas@gmail.com>
2023-12-11 19:54:11 -0300

    9c y 9d

---
+++ ciclos/ejercicio9c.py
@@ -0,0 +1,11 @@
+n=int(input('eliga un numero: '))
+suma = 0
+contador = 1
+
+while contador <= n:
+    if n % contador == 0:
+        suma = suma + 1
+        contador += 1
+    else:
+        contador += 1
+print('el numero ',n,' tiene ',suma,' divisores')
---
+++ ciclos/ejercicio9d.py
@@ -0,0 +1,16 @@
+n = int(input('eliga un numero: '))
+suma = 0
+contador = 1
+# for i in range(1,n+1):
+#     if 10 % i == 0:
+#         suma = suma + i
+# print(suma)
+
+while contador <= n:
+    if n % contador ==0:
+        suma = suma + contador
+        contador += 1
+    else:
+        contador += 1
+print(suma)
+
